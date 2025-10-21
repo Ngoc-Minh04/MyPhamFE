7818a0d441227e546e2f4e9c276b4c6f42e9a0f7
Ngoc-Minh04 <1150070029@sv.hcmunre.edu.vn>
2025-10-22 00:25:16 +0700

    123

--- Service/api.ts
+++ Service/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://192.168.40.17:5248/api", // ✅ đúng
+  baseURL: "http://192.168.40.17:5248", // ✅ đúng
   timeout: 15000,
   headers: {
     "Content-Type": "application/json",
